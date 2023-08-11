9eb7692598c859aa7317ba82b331dea17d909bb2
Rufat Khaslarov <rufat.khaslarov@orca.security>
2023-08-11 11:54:40 +0300

    update params

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import validateCodeOwners from './validator'
+import validateCodeOwners from './lib/validator'
 
 async function run(): Promise<void> {
   try {
